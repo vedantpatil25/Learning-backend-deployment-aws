e6782ce0d976cf5f82a21dfb05a5a6f2af5efb36
vedantpatil <vedantpatil2002@gmail.com>
2023-10-31 18:56:09 +0530

    1-checking the deployment

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,3 @@
 ssh -t -i "cert-file.cer" ubuntu@ec2-url  "sudo bash ~/deploy.sh"
+
+ssh -t -i "learning-deployment.pem" ubuntu@ec2-3-110-28-172.ap-south-1.compute.amazonaws.com "sudo bash ~/deploy.sh"

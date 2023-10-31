9f40d792a584e918f0ecf8a0f7db65b5970c42ab
vedantpatil <vedantpatil2002@gmail.com>
2023-10-31 19:03:05 +0530

    updated server for only / --> hello world

--- server/index.js
+++ server/index.js
@@ -9,12 +9,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/admin", adminRouter)
-app.use("/user", userRouter)
-
+// app.use("/admin", adminRouter)
+// app.use("/user", userRouter)
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
 
 // Connect to MongoDB
 // DONT MISUSE THIS THANKYOU!!
-mongoose.connect('mongodb://localhost:27017/courses', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
+// mongoose.connect('mongodb://localhost:27017/courses', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
